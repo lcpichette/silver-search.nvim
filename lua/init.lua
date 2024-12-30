6d5a90bf3998eb52734886fec90d1586c3861ab4
Lucas Pichette <lucascpichette@gmail.com>
2024-12-29 23:54:08 -0800

    Adds input

--- lua/init.lua
+++ lua/init.lua
@@ -6,6 +6,11 @@ function M.setup()
   vim.api.nvim_create_user_command("InstallThings", function()
     if M.instance == nil then
       M.instance = require("banana.instance").newInstance("core", "installer")
+      local el = M.instance:getElementById("input")
+      el:attachRemap("n", "<CR>", { "hover" }, function()
+        local input = vim.fn.input("Input text: ")
+        el:setTextContent(input)
+      end, {})
     end
     M.instance:open()
   end, {})
